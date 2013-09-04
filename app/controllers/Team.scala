d466ff77f146592cdb1bc793d5f927129c7dc685
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2013-09-04 03:16:17 -0400

    Added quotes and aliases; fleshing out Team operations

--- app/controllers/Team.scala
+++ app/controllers/Team.scala
@@ -2,12 +2,30 @@ package controllers
 
 import play.api.mvc.{Controller, Action}
 import models.Repository
+import play.api.data._
+import play.api.data.Forms._
 
 object Team extends Controller {
 
   import play.api.Play.current
 
   private val repo: Repository = new Repository(play.api.db.slick.DB.driver)
+
+  val teamForm: Form[models.Team] = Form(
+    mapping(
+      "id" -> longNumber,
+      "key" -> nonEmptyText,
+      "name" -> nonEmptyText,
+      "longName" -> nonEmptyText,
+      "nickname" -> nonEmptyText,
+      "primaryColor" -> optional(text),
+      "secondaryColor" -> optional(text),
+      "logoUrl" -> optional(text),
+      "officialUrl" -> optional(text),
+      "officialTwitter" -> optional(text)
+    )(models.Team.apply)(models.Team.unapply)
+  )
+
 
   def list = Action {
     implicit request =>
@@ -28,10 +46,37 @@ object Team extends Controller {
       }
   }
 
-  def create = TODO
-  def delete = TODO
-  def update = TODO
+  def update(key:String) = Action {
+    implicit request =>
+      play.api.db.slick.DB.withSession {
+      val oTeam: Option[models.Team] = repo.getTeam(key)
+      if (oTeam.isDefined) {
+        Ok(views.html.teamForm(teamForm.fill(oTeam.get),oTeam.get.name+" "+oTeam.get.nickname))
+      } else {
+        NotFound("bbbbb")
+      }
+    }
+  }
 
+  def create = TODO
+
+  def delete = TODO
+
+  def submit = Action {
+    implicit request =>
+      play.api.db.slick.DB.withSession {
+        import repo.profile.simple._
+
+        teamForm.bindFromRequest.fold(
+          errors => BadRequest(views.html.teamForm(errors, "Save failed")),
+          team => {
+            repo.u`
+            Ok(views.html.teamList(repo.getTeams))
+          }
+        )
+      } //FIXME bad url in address bar
+
+  }
 
 
 }
