352bca5705ea89822427bc77bf18f2f697197d44
fijimf <fijimf@gmail.com>
2013-09-05 17:54:59 -0400

    Create Conference.scala

---
+++ app/controllers/Conference.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api.mvc._
+
+object Conference extends Controller {
+
+  def list = TODO
+
+  def submit = TODO
+
+  def create = TODO
+
+  def view = TODO
+
+  def edit = TODO
+  
+  def delete = TODO
+
+}
