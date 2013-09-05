14a8c5f47e4e860b05e4548e82852435f6322c84
fijimf <fijimf@gmail.com>
2013-09-05 17:55:30 -0400

    Create Season.scala

---
+++ app/controllers/Season.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api.mvc._
+
+object Season extends Controller {
+
+  def list = TODO
+
+  def submit = TODO
+
+  def create = TODO
+
+  def view = TODO
+
+  def edit = TODO
+  
+  def delete = TODO
+
+}
