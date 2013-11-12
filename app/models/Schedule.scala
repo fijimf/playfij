a8ea034b7071eae705d164c9e94542a27c13f940
fijimf <fijimf@gmail.com>
2013-11-11 22:48:24 -0500

    Got schedules and results

--- app/models/Schedule.scala
+++ app/models/Schedule.scala
@@ -7,7 +7,7 @@ case class ScheduleDao(m: Model) {
 
   import m.profile.simple._
 
-  val fmt = DateTimeFormat.forPattern("yyyyMMdd");
+  val fmt = DateTimeFormat.forPattern("yyyyMMdd")
 
   def schedule(implicit s: scala.slick.session.Session): Schedule = {
     val teams: List[Team] = TeamDao(m).list
@@ -96,7 +96,8 @@ case class ScheduleDao(m: Model) {
       }
     }
 
-    Some(TeamPage(team, conference, season, schedule, results))
+
+    Some(TeamPage(team, conference, season, schedule, results, loadConference(conference.key, season.key)))
   }
 
   def season(d: LocalDate)(implicit s: scala.slick.session.Session): Option[Season] = {
@@ -107,14 +108,90 @@ case class ScheduleDao(m: Model) {
     season(new LocalDate).orElse(seasonQuery.list().sortBy(_.to.toDate).reverse.headOption)
   }
 
+  def loadConference(conferenceKey:String, seasonKey:String)(implicit s: scala.slick.session.Session):ConferenceStandings = {
+    val confData: Map[Conference, List[Team]] = (for {season <- seasonQuery if season.key === seasonKey
+                                                    conference <- conferenceQuery if conference.key === conferenceKey
+                                                    assoc <- assocQuery if assoc.seasonId === season.id && assoc.conferenceId === conference.id
+                                                    team <- teamQuery if team.id === assoc.teamId} yield (conference, team)).list().groupBy(_._1).mapValues(_.map(_._2))
+    val (conference, teams) = confData.head
+    val confTeamMap: Map[Team, Conference] = (for {season <- seasonQuery if season.key === seasonKey
+                                                   assoc <- assocQuery if assoc.seasonId === season.id
+                                                   team <- teamQuery if team.id === assoc.teamId
+                                                   conference <- conferenceQuery if conference.id === assoc.conferenceId} yield (team, conference)).list().toMap
+
+
+    val gameData = (for {result <- m.Results
+                         game <- result.gameFk
+                         homeTeam <- game.homeTeamFk
+                         awayTeam <- game.awayTeamFk} yield { (homeTeam, result.homeScore, awayTeam, result.awayScore) }).list()
+      .filter(tup=> teams.contains(tup._1) || teams.contains(tup._3))
+    val z = Map.empty[Team, (Record, Record)].withDefaultValue(Record(), Record())
+    val teamRecords: List[(Team, (Record, Record))] = gameData.foldLeft(z) {
+      case (map: Map[Team, (Record, Record)], tuple: (Team, Int, Team, Int)) => {
+        val (homeTeam, homeScore, awayTeam, awayScore) = tuple
+        (teams.contains(homeTeam), teams.contains(awayTeam)) match {
+          case (true, true) => {
+            if (homeScore > awayScore) {
+              val (hconfRecord, hoverallRecord) = map(homeTeam)
+              val hh = (hconfRecord ++, hoverallRecord ++)
+              val (aconfRecord, aoverallRecord) = map(awayTeam)
+              val aa = (aconfRecord --, aoverallRecord --)
+              map + (homeTeam -> hh) + (awayTeam -> aa)
+            } else {
+              val (hconfRecord, hoverallRecord) = map(homeTeam)
+              val hh = (hconfRecord --, hoverallRecord --)
+              val (aconfRecord, aoverallRecord) = map(awayTeam)
+              val aa = (aconfRecord ++, aoverallRecord ++)
+              map + (homeTeam -> hh) + (awayTeam -> aa)
+            }
+          }
+          case (true, false) => {
+            if (homeScore > awayScore) {
+              val (hconfRecord, hoverallRecord) = map(homeTeam)
+              val hh = (hconfRecord, hoverallRecord ++)
+              map + (homeTeam -> hh)
+            } else {
+              val (hconfRecord, hoverallRecord) = map(homeTeam)
+              val hh = (hconfRecord, hoverallRecord --)
+              map + (homeTeam -> hh)
+            }
+          }
+          case (false, true) => {
+            if (homeScore > awayScore) {
+              val (aconfRecord, aoverallRecord) = map(awayTeam)
+              val aa = (aconfRecord, aoverallRecord --)
+              map + (awayTeam -> aa)
+            } else {
+              val (aconfRecord, aoverallRecord) = map(awayTeam)
+              val aa = (aconfRecord, aoverallRecord ++)
+              map + (awayTeam -> aa)
+            }
+          }
+          case _ => map
+        }
+
+      }
+    }.toList
+
+    val sortedTeams: List[(Team, (Record, Record))] = teamRecords.sortWith((t1: (Team, (Record, Record)), t2: (Team, (Record, Record))) => {
+      val d = t1._2._1.compareTo(t2._2._1)
+      if (d == 0) {
+        t1._2._2.compareTo(t2._2._2) > 0
+      } else {
+        d > 0
+      }
+    })
+    ConferenceStandings(conference,sortedTeams)
+  }
 }
 
+case class ConferenceStandings(conference:Conference, records:List[(Team, (Record, Record))])
 
 case class ResultLine(date: LocalDate, opp: Team, versusOrAt: String, outcome: String, scores: String)
 
 case class ScheduleLine(date: LocalDate, opp: Team, versusOrAt: String)
 
-case class TeamPage(team: Team, conference: Conference, season: Season, schedule: List[ScheduleLine], results: List[ResultLine]) {
+case class TeamPage(team: Team, conference: Conference, season: Season, schedule: List[ScheduleLine], results: List[ResultLine], conferenceStandings:ConferenceStandings) {
 
 }
 
@@ -153,10 +230,24 @@ case object emptyStreak extends Streak {
   def -- : Streak = LossStreak(1)
 }
 
-case class Record(wins: Int = 0, losses: Int = 0, streak: Streak = emptyStreak) {
+case class Record(wins: Int = 0, losses: Int = 0, streak: Streak = emptyStreak) extends Comparable[Record]{
   def ++ : Record = Record(wins + 1, losses, streak ++)
 
   def -- : Record = Record(wins, losses + 1, streak --)
+
+  def compareTo(r: Record): Int = {
+    val d = (wins - losses) - (r.wins - r.losses)
+    if (d == 0) {
+      wins - r.wins
+    } else {
+      d
+    }
+  }
+
+  override def toString():String = {
+    "%d - %d".format(wins,losses)
+  }
+
 }
 
 case class RecordKey(teamKey: String, seasonKey: String, typeKey: String)
@@ -174,17 +265,17 @@ case object OverallRecord extends RecordDef {
 
 }
 
-case class ConferenceTest(teamToConf: Map[Season, Map[Team, Conference]], matchConf: Boolean) extends RecordDef {
-  private val idMap: Map[Long, Map[Long, Long]] = teamToConf.map {
-    case (season: Season, map: Map[Team, Conference]) => season.id -> map.map {
-      case (team: Team, conference: Conference) => (team.id, conference.id)
+  case class ConferenceTest(teamToConf: Map[Season, Map[Team, Conference]], matchConf: Boolean) extends RecordDef {
+    private val idMap: Map[Long, Map[Long, Long]] = teamToConf.map {
+      case (season: Season, map: Map[Team, Conference]) => season.id -> map.map {
+        case (team: Team, conference: Conference) => (team.id, conference.id)
+      }.toMap
     }.toMap
-  }.toMap
 
-  def label: String = if (matchConf) "Conference" else "Non-Conference"
+    def label: String = if (matchConf) "Conference" else "Non-Conference"
 
-  def include(g: Game): Boolean = idMap.get(g.seasonId) match {
-    case Some(lookup) => matchConf == (lookup.get(g.homeTeamId) == lookup.get(g.awayTeamId))
-    case _ => false
+    def include(g: Game): Boolean = idMap.get(g.seasonId) match {
+      case Some(lookup) => matchConf == (lookup.get(g.homeTeamId) == lookup.get(g.awayTeamId))
+      case _ => false
+    }
   }
-}
