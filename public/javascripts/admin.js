25ca43a440c567b210e74f262c134b7385f3f680
fijimf <fijimf@gmail.com>
2014-01-16 00:44:52 -0500

    Fleshing out teams page

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -12,3 +12,15 @@ function loadQuote() {
         }
     );
 }
+
+function headlineTeam(src, teamName, nickname, confName, confWins, confLosses, wins, losses) {
+    $("#headline").html("<h3><img style='height:80px; width:80px;' src='"+src+"' />"+teamName+"&nbsp;"+nickname+"</h3><h4>("+wins+"-"+losses+", "+confWins+"- "+confLosses+" "+confName+" )</h4>");
+}
+
+function headlineConf(src, confName) {
+    $("#headline").html("<h3><img style='height:80px; width:80px;' src='"+src+"' />&nbsp;</h3><h4>"+confName+"</h4>");
+}
+
+function dropHeadline() {
+    $("#headline").html("<h3><img style='height:80px; width:80px;' src='/assets/images/blank.png' />&nbsp;</h3><h4>&nbsp;</h4>");
+}
