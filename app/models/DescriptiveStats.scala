e8b74db7a6636e36da5067b6728852419504e8ed
fijimf <fijimf@gmail.com>
2014-02-20 22:10:48 -0500

    Minor cosmetic fixes on stat page

--- app/models/DescriptiveStats.scala
+++ app/models/DescriptiveStats.scala
@@ -8,7 +8,7 @@ case class DescriptiveStats(n:Int, mean:Double, stdDev:Double, skew:Double, kurt
 object DescriptiveStats {
   def apply(series: Series[_, Double]): DescriptiveStats = {
     (series.length, series.min, series.max) match {
-      case (n, Some(min), Some(max)) => DescriptiveStats(n, series.mean, series.stdev, series.skew, series.kurt, min, series.percentile(0.25, PctMethod.NIST), series.median, series.percentile(0.75, PctMethod.NIST), max)
+      case (n, Some(min), Some(max)) => DescriptiveStats(n, series.mean, series.stdev, series.skew, series.kurt, min, series.percentile(25.0, PctMethod.NIST), series.median, series.percentile(75.0, PctMethod.NIST), max)
       case (n, _, _) => DescriptiveStats(0, Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN)
     }
   }
